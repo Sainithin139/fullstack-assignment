8acefd663b94fbdeaaf48d9366c6cc5f623d9d5e
Sainithin139 <151899339+Sainithin139@users.noreply.github.com>
2024-12-20 21:25:42 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'complaints_db',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'complaints',  # Your app
+]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_password'
