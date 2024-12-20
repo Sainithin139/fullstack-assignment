7acabe758f070d0e302779718f8b63ff1cd4bc13
Sainithin139 <151899339+Sainithin139@users.noreply.github.com>
2024-12-20 21:34:23 +0530

    Create Complaint.js

---
+++ Complaint.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const complaintSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true, enum: ["Product", "Service", "Support"] },
+  priority: { type: String, required: true, enum: ["Low", "Medium", "High"] },
+  status: { type: String, default: "Pending", enum: ["Pending", "In Progress", "Resolved"] },
+  dateSubmitted: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("Complaint", complaintSchema);
